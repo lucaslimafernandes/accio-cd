062ad239c6fdb48bc007334b46f21af5fbea501c
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-11-08 20:17:33 -0300

    First route

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/lucaslimafernandes/accio-cd/router"
+)
 
 func main() {
 
 	fmt.Println("accio-cd")
+
+	app := fiber.New()
+	app.Use(cors.New())
+
+	router.SetupRoutes(app)
 
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,13 @@
 package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/lucaslimafernandes/accio-cd/handler"
+)
+
+func SetupRoutes(app *fiber.App) {
+
+	api := app.Group("/api")
+	api.Get("/", handler.Hello)
+
+}
