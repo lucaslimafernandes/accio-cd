d8c33c00716913279012fe04ca35f4b1639ca631
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-11-21 10:51:21 -0300

    git clone test OK

--- handler/push.go
+++ handler/push.go
@@ -4,16 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/lucaslimafernandes/accio-cd/utils"
 )
 
 func EventPush(cdFile *utils.CDRunfile) {
-
 	fmt.Println("EventPush")
 
 	for _, v := range cdFile.Tasks {
-
 		var cmd string
 		fmt.Printf("Executando comando %v...\n", v.Command)
 
@@ -24,13 +23,17 @@ func EventPush(cdFile *utils.CDRunfile) {
 			cmd = v.Command
 		}
 
-		lsCmd := exec.Command(cmd)
-		lsOutput, err := lsCmd.Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Saída do comando %v\n:", v.Command)
-		fmt.Println(string(lsOutput))
+		parts := strings.Fields(cmd)
+		args := parts[1:]
+		cmdExec := exec.Command(parts[0], args...)
+		cmdExec.Dir = cdFile.LocalPath // Define o diretório de trabalho para o comando
 
+		// Executando o comando
+		cmdOutput, err := cmdExec.CombinedOutput() // Usando CombinedOutput para capturar stdout e stderr
+		if err != nil {
+			log.Fatalf("Erro ao executar o comando '%s' em '%s': %v\nSaída: %s", v.Command, cdFile.LocalPath, err, string(cmdOutput))
+		}
+		fmt.Printf("Saída do comando %v:\n", v.Command)
+		fmt.Println(string(cmdOutput))
 	}
 }
